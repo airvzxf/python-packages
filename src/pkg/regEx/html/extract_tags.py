19220038795c06c35fedef5603e3146f698a1f53
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-01 18:40:36 -0400

    Changed the names of the tests; Fixed the problem to search in multilines text with re.DOTALL; Refactored the production code with a private function.

--- src/pkg/regEx/html/extract_tags.py
+++ src/pkg/regEx/html/extract_tags.py
@@ -4,22 +4,30 @@
 import re
 
 
-def get_from_tag(tag=None, text=""):
+def get_from_tag(tag=None, text="", multi_matches=False):
     if tag is None:
         return
 
-    # RegEx
-    # https://regex101.com/r/aYOgRZ/2
-    reg_ex_string = r"<{0}[^<]+?>(.*)</{0}>".format(tag)
-    pattern_get_from_tag = re.compile(reg_ex_string, re.MULTILINE)
-    matches = re.search(pattern_get_from_tag, text)
-
-    if matches is not None:
-        # return matches.index(1)
-        return matches.group(1)
+    if multi_matches:
+        return []
+    else:
+        return __search_from_tag_reg_ex(tag, text)
 
     return
 
+
+def __search_from_tag_reg_ex(tag=None, text=""):
+    if tag is None:
+        return
+
+    reg_ex_string = r"<{0}[^<]+?>(.*)</{0}>".format(tag)
+    pattern_get_from_tag = re.compile(reg_ex_string, re.DOTALL)
+    matches = re.search(pattern_get_from_tag, text)
+
+    if matches is not None:
+        return matches.group(1)
+
+    return
 
 def get_from_comment(text=""):
     print("text: ", text)
--- src/pkg/regEx/html/test_extract_tags.py
+++ src/pkg/regEx/html/test_extract_tags.py
@@ -7,10 +7,10 @@ from src.pkg.regEx.html.extract_tags import get_from_tag
 
 
 class TestGetFromTag(unittest.TestCase):
-    """Tests for `extract_tags.py`."""
+    """Tests for extract html tags"""
 
     # Test get_from_tag
-    def test_when_tag_param_is_nil(self):
+    def test_when_the_tag_param_is_nil(self):
         """Returns nothing"""
         self.assertIsNone(get_from_tag(None))
 
@@ -20,12 +20,12 @@ class TestGetFromTag(unittest.TestCase):
         expected_text = 'This is a test'
         self.assertEqual(actual_text, expected_text)
 
-    def test_when_html_code_not_contain_the_tag(self):
+    def test_when_the_html_code_not_contain_the_tag(self):
         """Returns None"""
         actual_text = get_from_tag('div', '<span name="test">This is a test</span>')
         self.assertIsNone(actual_text)
 
-    def test_when_exists_other_tags_with_the_same_name(self):
+    def test_when_exists_other_tags_with_the_same_name_inside_the_text(self):
         """Returns the content for the first and the last tag"""
         text = '<article><div name="my"><span>My test</span><div name="f"><span>footer<span></div></div></article>'
         expected_text = '<span>My test</span><div name="f"><span>footer<span></div>'
@@ -33,48 +33,86 @@ class TestGetFromTag(unittest.TestCase):
 
         self.assertEqual(actual_text, expected_text)
 
-    # def test_when_text_has_breaklines(self):
-    #     """Returns the content for the first and the last tag"""
-    #     text = """
-    #         <article>
-    #             <div name="my">
-    #                 <span>My test</span>
-    #                 <div name="f">
-    #                     <span>footer<span>
-    #                 </div>
-    #             </div>
-    #         </article>
-    #     """
-    #     expected_text = """<span>My test</span>
-    #                 <div name="f">
-    #                     <span>footer<span>
-    #                 </div>"""
-    #     actual_text = get_from_tag('div', text)
-    #
-    #     self.assertEqual(actual_text, expected_text)
+    def test_when_the_text_has_break_lines(self):
+        """Returns the content for the first and the last tag"""
+        text = """
+                <article>
+                    <div name="my">
+                        <span>My test</span>
+                        <div name="f">
+                            <span>footer<span>
+                        </div>
+                    </div>
+                </article>"""
 
-    # def test_when_text_has_breaklines(self):
-    #     """Returns the content for the first and the last tag"""
-    #     text = """
-    #         <article>
-    #             <div name="div1">
-    #                 <span name="test">This is a test</span>
-    #             </div>
-    #         </article>
-    #         <div name="footer">
-    #             <span>My footer<span>
-    #         </div>
-    #     """
-    #     expected_text = """
-    #                 <span name="test">This is a test</span>
-    #             </div>
-    #         </article>
-    #         <div name="footer">
-    #             <span>My footer<span>
-    #     """
-    #     actual_text = get_from_tag('div', text)
-    #
-    #     self.assertEqual(actual_text, expected_text)
+        expected_text = """
+                        <span>My test</span>
+                        <div name="f">
+                            <span>footer<span>
+                        </div>
+                    """
+
+        actual_text = get_from_tag('div', text)
+
+        self.assertEqual(actual_text, expected_text)
+
+    def test_when_text_has_break_lines_and_should_be_return_more_than_one_result(self):
+        """Returns two results"""
+        # text, expected_text_1, expected_text_2 = get_two_expected_matches()
+        #
+        # result = get_from_tag('div', text, True)
+        #
+        # print("result: ", result)
+        #
+        # self.assertEqual(self, 2, len(result))
+        # self.assertEqual(result[0], expected_text_1)
+        # self.assertEqual(result[2], expected_text_2)
+        pass
+
+        # """Return the three articles"""
+        # text = """
+        #     <article>
+        #         <span>Article #1</span>
+        #     </article>
+        #     <div>Other:</div>
+        #     <br>
+        #     <article>
+        #         <span>Article #2</span>
+        #         <div class="line-red">Line red</div>
+        #     </article>
+        #     <div>Other:</div>
+        #     <br>
+        #     <article>
+        #         <span>Article #3</span>
+        #     </article>
+        # """
+
+
+def get_two_expected_matches():
+    """Returns the text and the two expected strings"""
+    text = """
+            <article>
+                <div name="div1">
+                    <span name="test">This is a test</span>
+                    <div class="line-red">Line #1</div>
+                </div>
+            </article>
+            <div name="footer">
+                <span>My footer<span>
+                <div class="line-green">Line #2</div>
+            </div>"""
+
+    expected_text_1 = """
+                    <span name="test">This is a test</span>
+                    <div class="line-red">Line #1</div>
+                """
+
+    expected_text_2 = """
+                    <span>My footer<span>
+                    <div class="line-green">Line #2</div>
+                """
+
+    return text, expected_text_1, expected_text_2
 
 
 if __name__ == '__main__':
