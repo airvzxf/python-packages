30275b4cc460f88a221d98604fde92a9da5cb466
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-04 15:39:46 -0400

    Create function to find the tags and their content but if exists more tags outside the first tag it will look for them; Spell improvements; Prepare the test for find tags and but only get the content inside

--- src/pkg/regEx/html/extract_tags.py
+++ src/pkg/regEx/html/extract_tags.py
@@ -4,28 +4,83 @@
 import re
 
 
-def get_from_tag(tag=None, text="", ignore_case=True, multi_matches=False):
+def get_from_tag(tag=None, text="", ignore_case=True, multi_matches=False, get_only_content_inside=False):
     if tag is None:
         return
 
     if multi_matches:
-        return []
+        return __search_html_sub_tags(tag, text, ignore_case, get_only_content_inside)
     else:
-        return __search_from_tag_reg_ex(tag, text, ignore_case)
-
-    return
+        return __search_from_tag_reg_ex(tag, text, ignore_case, get_only_content_inside)
 
 
-def __search_from_tag_reg_ex(tag=None, text="", ignore_case=True):
+def __search_html_sub_tags(tag=None, text="", ignore_case=True, get_only_content_inside=False):
     if tag is None:
         return
 
-    reg_ex_string = r"<{0}[^<]+?>(.*)</{0}>".format(tag)
+    flags = re.RegexFlag.DOTALL
 
     if ignore_case:
-        pattern_get_from_tag = re.compile(reg_ex_string, re.DOTALL | re.IGNORECASE)
-    else:
-        pattern_get_from_tag = re.compile(reg_ex_string, re.DOTALL)
+        flags = flags | re.RegexFlag.IGNORECASE
+
+    reg_ex_start_tag = re.compile(r"<{0}[^<]*?>".format(tag), flags)
+    reg_ex_end_tag = re.compile(r"</{0}[^<]*?>".format(tag), flags)
+
+    tag_matrix = {}
+    opened = re.finditer(reg_ex_start_tag, text)
+    for o in opened:
+        print("Opened start/end at {}: {}".format(o.span(), o.group()))
+        tag_matrix[o.start()] = {'value': 1, 'start_at': o.start(), 'end_at': o.end()}
+    print("")
+    closed = re.finditer(reg_ex_end_tag, text)
+    for c in closed:
+        print("Closed start/end at {}: {}".format(c.span(), c.group()))
+        tag_matrix[c.start()] = {'value': -1, 'start_at': c.start(), 'end_at': c.end()}
+    print("")
+
+    tag_matrix = sorted(tag_matrix.items())
+    for tm in tag_matrix:
+        print("tag_matrix: ", tm)
+    print("")
+
+    tags_found = []
+    text_star_at = 0
+    tags_opened = 0
+    is_a_new_tag = True
+
+    for _, tag in tag_matrix:
+        if tags_opened == 0 and is_a_new_tag:
+            text_star_at = tag.get('start_at')
+            is_a_new_tag = False
+
+        tags_opened += tag.get('value')
+        if tags_opened == 0:
+            tags_found.append(text[text_star_at: tag.get('end_at')])
+            is_a_new_tag = True
+
+    print("text:\n", text)
+    for tf in tags_found:
+        print("tags_found:")
+        print("-----------------------")
+        print(tf)
+        print("-----------------------\n")
+    print("\n")
+
+    return tags_found
+
+
+def __search_from_tag_reg_ex(tag=None, text="", ignore_case=True, get_only_content_inside=False):
+    if tag is None:
+        return
+
+    flags = re.RegexFlag.DOTALL
+
+    if ignore_case:
+        flags = flags | re.RegexFlag.IGNORECASE
+
+    reg_ex_html_tag = r"<{0}[^<]*?>(.*)</{0}>".format(tag)
+
+    pattern_get_from_tag = re.compile(reg_ex_html_tag, flags)
 
     matches = re.search(pattern_get_from_tag, text)
 
@@ -35,6 +90,21 @@ def __search_from_tag_reg_ex(tag=None, text="", ignore_case=True):
     return
 
 
-def get_from_comment(text=""):
-    print("text: ", text)
-    return
+# TODO: Move this function to other package which show the object's methods
+def __show_methods(main_object=None, execute=True):
+    if main_object is None:
+        return
+
+    print("{}".format(main_object))
+    print("{}".format(dir(main_object)))
+
+    if execute:
+        for m in dir(main_object):
+            try:
+                print("object.{}(): {}".format(m, eval("main_object.{}()".format(m))))
+            except AttributeError:
+                pass
+            except TypeError:
+                pass
+            except StopIteration:
+                pass
--- src/pkg/regEx/html/test_extract_tags.py
+++ src/pkg/regEx/html/test_extract_tags.py
@@ -67,40 +67,92 @@ class TestGetFromTag(unittest.TestCase):
 
         self.assertEqual(actual_text, expected_text)
 
-    def test_when_text_has_break_lines_and_should_be_return_more_than_one_result(self):
+    def test_when_text_has_break_lines_and_should_be_return_two_results(self):
         """Returns two results"""
-        # text, expected_text_1, expected_text_2 = get_two_expected_matches()
-        #
-        # result = get_from_tag('div', text, True)
-        #
-        # print("result: ", result)
-        #
-        # self.assertEqual(self, 2, len(result))
-        # self.assertEqual(result[0], expected_text_1)
-        # self.assertEqual(result[2], expected_text_2)
-        pass
+        text, expected_text_1, expected_text_2 = get_two_expected_matches()
 
-        # """Return the three articles"""
-        # text = """
-        #     <article>
-        #         <span>Article #1</span>
-        #     </article>
-        #     <div>Other:</div>
-        #     <br>
-        #     <article>
-        #         <span>Article #2</span>
-        #         <div class="line-red">Line red</div>
-        #     </article>
-        #     <div>Other:</div>
-        #     <br>
-        #     <article>
-        #         <span>Article #3</span>
-        #     </article>
-        # """
+        result = get_from_tag('div', text, True, True)
+
+        self.assertEqual(2, len(result))
+        self.assertEqual(result[0], expected_text_1)
+        self.assertEqual(result[1], expected_text_2)
+
+    # def test_when_text_has_break_lines_and_should_be_return_two_results_and_only_the_content_inside_of_the_tag(self):
+    #     """Returns two results"""
+    #     text, expected_text_1, expected_text_2 = get_two_expected_matches_and_only_the_content_inside_of_the_tag()
+    #
+    #     result = get_from_tag('div', text, True, True, True)
+    #
+    #     print("len(result): ", len(result))
+    #     print("#1:")
+    #     print("-------------------------------------------------")
+    #     print(result[0])
+    #     print("-------------------------------------------------")
+    #     print(expected_text_1)
+    #     print("-------------------------------------------------")
+    #     print("#2:")
+    #     print("-------------------------------------------------")
+    #     print(result[1])
+    #     print("-------------------------------------------------")
+    #     print(expected_text_2)
+    #     print("-------------------------------------------------")
+    #
+    #     self.assertEqual(2, len(result))
+    #     self.assertEqual(result[0], expected_text_1)
+    #     self.assertEqual(result[1], expected_text_2)
+
+    def test_when_text_has_break_lines_and_should_be_return_three_results(self):
+        """Return the three articles"""
+        text, expected_text_1, expected_text_2, expected_text_3 = get_three_expected_matches()
+
+        result = get_from_tag('article', text, True, True)
+
+        self.assertEqual(3, len(result))
+        self.assertEqual(result[0], expected_text_1)
+        self.assertEqual(result[1], expected_text_2)
+        self.assertEqual(result[2], expected_text_3)
+
+        # def test_when_text_has_break_lines_and_should_be_return_three_results_and_only_the_content_inside_of_the_tag(self):
+        #     """Return the three articles"""
+        #     text, expected_text_1, expected_text_2, expected_text_3 = get_three_expected_matches_and_only_the_content_inside_of_the_tag()
+        #
+        #     result = get_from_tag('article', text, True, True, True)
+        #
+        #     self.assertEqual(3, len(result))
+        #     self.assertEqual(result[0], expected_text_1)
+        #     self.assertEqual(result[1], expected_text_2)
+        #     self.assertEqual(result[2], expected_text_3)
 
 
 def get_two_expected_matches():
     """Returns the text and the two expected strings"""
+    text = """
+            <article>
+                <div name="div1">
+                    <span name="test">This is a test</span>
+                    <div class="line-red">Line #1</div>
+                </div>
+            </article>
+            <div name="footer">
+                <span>My footer<span>
+                <div class="line-green">Line #2</div>
+            </div>"""
+
+    expected_text_1 = """<div name="div1">
+                    <span name="test">This is a test</span>
+                    <div class="line-red">Line #1</div>
+                </div>"""
+
+    expected_text_2 = """<div name="footer">
+                <span>My footer<span>
+                <div class="line-green">Line #2</div>
+            </div>"""
+
+    return text, expected_text_1, expected_text_2
+
+
+def get_two_expected_matches_and_only_the_content_inside_of_the_tag():
+    """Returns the text and the two expected strings but this strings are the content inside of the matcher tag"""
     text = """
             <article>
                 <div name="div1">
@@ -124,6 +176,76 @@ def get_two_expected_matches():
                 """
 
     return text, expected_text_1, expected_text_2
+
+
+def get_three_expected_matches():
+    """Returns the text and the three expected strings"""
+    text = """
+        <article>
+            <span>Article #1</span>
+        </article>
+        <div>Other:</div>
+        <br>
+        <article>
+            <span>Article #2</span>
+            <div class="line-red">Line red</div>
+        </article>
+        <div>Other:</div>
+        <br>
+        <article>
+            <span>Article #3</span>
+        </article>
+    """
+
+    expected_text_1 = """<article>
+            <span>Article #1</span>
+        </article>"""
+
+    expected_text_2 = """<article>
+            <span>Article #2</span>
+            <div class="line-red">Line red</div>
+        </article>"""
+
+    expected_text_3 = """<article>
+            <span>Article #3</span>
+        </article>"""
+
+    return text, expected_text_1, expected_text_2, expected_text_3
+
+
+def get_three_expected_matches_and_only_the_content_inside_of_the_tag():
+    """Returns the text and the three expected strings"""
+    text = """
+        <article>
+            <span>Article #1</span>
+        </article>
+        <div>Other:</div>
+        <br>
+        <article>
+            <span>Article #2</span>
+            <div class="line-red">Line red</div>
+        </article>
+        <div>Other:</div>
+        <br>
+        <article>
+            <span>Article #3</span>
+        </article>
+    """
+
+    expected_text_1 = """
+            <span>Article #1</span>
+        """
+
+    expected_text_2 = """
+            <span>Article #2</span>
+            <div class="line-red">Line red</div>
+        """
+
+    expected_text_3 = """
+            <span>Article #3</span>
+        """
+
+    return text, expected_text_1, expected_text_2, expected_text_3
 
 
 if __name__ == '__main__':
