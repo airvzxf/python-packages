b767eee325c05f4fc46e7aed5ad62bbb82475e11
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-15 15:32:01 -0400

    Renamed the parameters in the funciton show_available; Escape the dir() method if available is true; Added exception to IndexError.

--- src/pkg/debug/method/show_available.py
+++ src/pkg/debug/method/show_available.py
@@ -3,21 +3,34 @@
 """Print all the methods, and also the available methods in a specific object."""
 
 
-# TODO: Create Unit Test and DocStrings.
-def show_available(main_object=None, show_all=True, show_available=True):
+def show_available(main_object=None, print_all=True, print_available=True):
+    """Show available methods for an object which is debugging.
+
+    This function NOT return any value but it print in the console the methods
+
+    :param main_object: Object with is debugging
+    :param print_all: Print all methods with dir(object)
+    :param print_available: Try to execute all methods for object for example: object.reverse()
+    :return: None
+    """
     if main_object is None:
         return
 
     print("{}".format(main_object))
 
-    if show_all:
+    if print_all:
         print("{}".format(dir(main_object)))
 
-    if show_available:
+    if print_available:
         for m in dir(main_object):
+            if m == "__dir__":
+                continue
+
             try:
                 print("object.{}(): {}".format(m, eval("main_object.{}()".format(m))))
             except AttributeError:
+                pass
+            except IndexError:
                 pass
             except TypeError:
                 pass
