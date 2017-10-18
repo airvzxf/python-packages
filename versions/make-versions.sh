7a33c7df724c660965f9afdf5f1cdd5ff156ac1c
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-17 22:08:45 -0400

    Fixed vars without brackets in the bash file.

--- versions/make-versions.sh
+++ versions/make-versions.sh
@@ -8,10 +8,10 @@ file_version="${source_directory}pkg_v${now_utf}.tar.gz"
 file_last_version="${source_directory}pkg_last_version.tar.gz"
 
 # Clean all
-find $src_directory -type d -iname __pycache__ -exec rm -fr {} \;
-find $src_directory -type f -iname *.pyc* -exec rm -fr {} \;
-rm -f $file_last_version
+find ${src_directory} -type d -iname __pycache__ -exec rm -fr {} \;
+find ${src_directory} -type f -iname *.pyc* -exec rm -fr {} \;
+rm -f ${file_last_version}
 
 # Create file versions
-tar -zcf $file_version -C $src_directory ./pkg/
-cp $file_version $file_last_version
+tar -zcf ${file_version} -C ${src_directory} ./pkg/
+cp ${file_version} ${file_last_version}
