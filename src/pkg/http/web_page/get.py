2b7019960c3836feab233d20c591590069931735
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-22 02:14:33 -0400

    Added get html from a web link.

--- src/pkg/http/web_page/get.py
+++ src/pkg/http/web_page/get.py
@@ -3,8 +3,21 @@
 
 """Module: Get web page from http protocol."""
 
+from urllib3 import PoolManager
+
 
 def get(url=None):
     """Request the method GET to the http protocol."""
     if url is None:
         return None
+
+    http = PoolManager()
+    response = http.urlopen(method='GET', url=url)
+
+    return {
+        'status': response.status,
+        'headers': response.headers,
+        'data': response.data,
+        'reason': response.reason,
+        'decode_content': response.decode_content
+    }
--- src/pkg/http/web_page/test/test_get.py
+++ src/pkg/http/web_page/test/test_get.py
@@ -5,7 +5,52 @@
 
 from unittest import TestCase
 
+from urllib3_mock import Responses
+
+from pkg.http.web_page.get import get
+
+responses = Responses('requests.packages.urllib3')
+
 
 class TestPkgHttpWebPageGet(TestCase):
-    def test_get(self):
-        self.fail()
+    """Test the get request from web page with the http protocol."""
+
+    def test_if_url_is_none_return_noe(self):
+        """Return None if the parameter url is none."""
+        result = get()
+
+        self.assertIsNone(result)
+
+    @responses.activate
+    def test_the_request_parameters(self):
+        responses.add(method='GET', url='/')
+
+        get(url='http://www.fake_url.com')
+
+        self.assertEqual(1, len(responses.calls))
+
+        request = responses.calls[0].request
+        self.assertEqual('GET', request.method)
+        self.assertEqual('http', request.scheme)
+        self.assertEqual('www.fake_url.com', request.host)
+        self.assertEqual('/', request.url)
+
+    @responses.activate
+    def test_the_response_match_with_the_returned_dictionary(self):
+        responses.add(
+            method='GET',
+            url='/',
+            body='this is the body',
+            status=299,
+            content_type='text/html'
+        )
+
+        response = get(url='http://www.fake_url.com')
+
+        self.assertEqual(1, len(responses.calls))
+
+        self.assertEqual(299, response.get('status'))
+        self.assertEqual('text/html', response.get('headers').getheaders('Content-Type')[0])
+        self.assertEqual(b'this is the body', response.get('data'))
+        self.assertEqual(None, response.get('reason'))
+        self.assertEqual(True, response.get('decode_content'))
