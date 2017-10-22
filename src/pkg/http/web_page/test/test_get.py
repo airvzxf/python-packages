c02962f64a5ec24a70073cb818a3c7c6b554aedc
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-21 21:13:38 -0400

    Added a new test for get web page from http protocol.

---
+++ src/pkg/http/web_page/test/test_get.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""Test case for get the web page from the http protocol."""
+
+from unittest import TestCase
+
+
+class TestPkgHttpWebPageGet(TestCase):
+    def test_get(self):
+        self.fail()
