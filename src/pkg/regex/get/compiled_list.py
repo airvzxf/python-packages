bdc15ba98bd688a9bedabf7ed91f3461af7b509e
Israel Roldan <israel.alberto.rv@gmail.com>
2017-10-29 10:42:22 -0400

    Added a parameter in compiled list in RegEx package which returns the object if the the compiled list only have one result.

--- src/pkg/regex/get/compiled_list.py
+++ src/pkg/regex/get/compiled_list.py
@@ -8,20 +8,24 @@ Get a list of RegEx patterns and return a compiled list.
 from re import compile
 
 
-def compiled_list(patterns=None, flags=0):
+def compiled_list(patterns=None, flags=0, is_only_one=True):
     """
     Return a list with the compiled patterns.
 
     :param patterns: List with the patterns.
     :param flags: Flags options for the compile function.
+    :param is_only_one: If the result is only one object it returns the object versus the list with one element.
     :return: List with the compiled patterns.
     """
-    if patterns is None:
-        return []
-
     result_list = []
+
+    if patterns is None:
+        return result_list
 
     for pattern in patterns:
         result_list.append(compile(pattern=pattern, flags=flags))
+
+    if is_only_one and len(result_list) == 1:
+        return result_list[0]
 
     return result_list
--- src/pkg/regex/get/test/test_get_compile.py
+++ src/pkg/regex/get/test/test_get_compile.py
@@ -28,7 +28,7 @@ class TestPkgRegExGetCompiledList(TestCase):
         """
         Returns a basic list and doesn't send any flags.
         """
-        result_list = compiled_list([".*", "[A-Z]"])
+        result_list = compiled_list(patterns=[".*", "[A-Z]"])
 
         expected_list = [compile(".*"),
                          compile("[A-Z]")]
@@ -39,9 +39,30 @@ class TestPkgRegExGetCompiledList(TestCase):
         """
         Returns a basic list and the RegEx flags were sent.
         """
-        result_list = compiled_list([".*", "[A-Z]"], DOTALL | IGNORECASE)
+        result_list = compiled_list(patterns=[".*", "[A-Z]"],
+                                    flags=DOTALL | IGNORECASE)
 
         expected_list = [compile(".*", DOTALL | IGNORECASE),
                          compile("[A-Z]", DOTALL | IGNORECASE)]
+
+        self.assertEqual(expected_list, result_list)
+
+    def test_sending_one_pattern_return_object_vs_list(self):
+        """
+        Returns the one compiled RegEx object.
+        """
+        result_list = compiled_list(patterns=[".*"])
+
+        expected_list = compile(".*")
+
+        self.assertEqual(expected_list, result_list)
+
+    def test_sending_one_pattern_return_list_with_one_compiled_regex(self):
+        """
+        Returns a list with one compiled RegEx.
+        """
+        result_list = compiled_list(patterns=[".*"], is_only_one=False)
+
+        expected_list = [compile(".*")]
 
         self.assertEqual(expected_list, result_list)
