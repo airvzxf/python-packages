13cba785b3b3a799ccc65c3ab2710227eba789f4
Israel Roldan <israel.alberto.rv@gmail.com>
2017-11-11 13:08:42 -0500

    Added quotes to all vars just because it's a recomendation for the pyChar inspector.

--- versions/make-versions.sh
+++ versions/make-versions.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Setup variables
-now_utf=$(date -u +"%Y.%m.%d.%H.%M.%S.%N")
+now_utf="$(date -u +"%Y.%m.%d.%H.%M.%S.%N")"
 source_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
 src_directory="${source_directory}../src/"
 file_version="${source_directory}pkg_v${now_utf}.tar.gz"
 file_last_version="${source_directory}pkg_last_version.tar.gz"
 
 # Clean all
-find ${src_directory} -type d -iname __pycache__ -exec rm -fr {} \;
-find ${src_directory} -type f -iname *.pyc* -exec rm -fr {} \;
-rm -f ${file_last_version}
+find "${src_directory}" -type d -iname __pycache__ -exec rm -fr "{}" \;
+find "${src_directory}" -type f -iname *.pyc* -exec rm -fr "{}" \;
+rm -f "${file_last_version}"
 
 # Create file versions
-tar -zcf ${file_version} -C ${src_directory} ./pkg/
-cp ${file_version} ${file_last_version}
+tar -zcf "${file_version}" -C "${src_directory}" ./pkg/
+cp "${file_version}" "${file_last_version}"
