eca811c9ccdebf5ba6133b67cf0df9ffbe7265c5
Israel Roldan <israel.alberto.rv@gmail.com>
2017-11-11 21:47:40 -0500

    Added extract test for tar gz files in the O.S.

--- src/pkg/os/file/type/tar_gz/extract.py
+++ src/pkg/os/file/type/tar_gz/extract.py
@@ -4,3 +4,26 @@
 """
 Module: Extract content from the files Gzip or Tar Gz in the Operative System.
 """
+
+from tarfile import TarError, open as open_tarfile
+
+
+def extract(path: str = None, extract_path: str = None) -> bool:
+    """
+    Extract Tar Gz file in the operative system.
+
+    :param path: The directory path where the tar gz file exists.
+    :param extract_path: The directory path where the tar gz would be extracted.
+    :return: True if it was extracted or false if it isn't.
+
+    :rtype: bool
+    """
+    if not path or not extract_path:
+        return False
+
+    try:
+        with open_tarfile(path, 'r:gz') as tar_gz_file:
+            tar_gz_file.extractall(extract_path)
+            return True
+    except (OSError, TarError):
+        return False
--- src/pkg/os/file/type/tar_gz/test/test_extract.py
+++ src/pkg/os/file/type/tar_gz/test/test_extract.py
@@ -4,3 +4,94 @@
 """
 Test case for extract content from Gzip file.
 """
+
+from unittest import TestCase
+
+from mock import Mock, patch
+
+from pkg.os.file.type.tar_gz.extract import TarError, extract
+
+
+class TestPkgOsFileTypeTarGzExtract(TestCase):
+    """
+    Test to extract a tar gz file form the operative system.
+    """
+
+    def test_the_default_parameters(self):
+        """
+        Return false if it send the default parameters that include directory to none.
+        """
+        extracted = extract()
+
+        self.assertEqual(False, extracted)
+
+    def test_if_the_path_is_empty(self):
+        """
+        Return false if it path is empty.
+        """
+        extracted = extract(path='')
+
+        self.assertEqual(False, extracted)
+
+    def test_if_the_extract_path_is_empty(self):
+        """
+        Return false if it extract path is empty.
+        """
+        extracted = extract(path='fake_path', extract_path='')
+
+        self.assertEqual(False, extracted)
+
+    @patch('pkg.os.file.type.tar_gz.extract.open_tarfile')
+    def test_if_the_open_file_raise_any_OSError(self, mock_open_tarfile):
+        """
+        Return false if the file does't exist or any OSError.
+        """
+        mock_open_tarfile.side_effect = OSError
+
+        extracted = extract(path='fake_file.tar.gz')
+
+        self.assertEqual(False, extracted)
+
+    @patch('pkg.os.file.type.tar_gz.extract.open_tarfile')
+    def test_if_the_file_exists_and_was_opened(self, mock_open_tarfile):
+        """
+        Check if the file exist and was opened that means the method open_tarfile was called.
+        """
+        extract(path='fake_file.tar.gz', extract_path='./')
+
+        mock_open_tarfile.assert_called_once_with('fake_file.tar.gz', 'r:gz')
+
+    @patch('pkg.os.file.type.tar_gz.extract.open_tarfile')
+    def test_if_the_file_was_not_extracted(self, mock_open_tarfile):
+        """
+        Check if the file is empty, it's not a tar file or any Tar Error.
+        """
+        mocked_tar_gz = Mock()
+        mock_open_tarfile.return_value.__enter__.side_effect = TarError
+
+        extract(path='fake_file.tar.gz', extract_path='./')
+
+        mocked_tar_gz.extractall.assert_not_called()
+
+    @patch('pkg.os.file.type.tar_gz.extract.open_tarfile')
+    def test_if_the_file_was_extracted(self, mock_open_tarfile):
+        """
+        Check if the file was extracted that means the method extractall was called.
+        """
+        mocked_tar_gz = Mock()
+        mock_open_tarfile.return_value.__enter__.return_value = mocked_tar_gz
+
+        extract(path='fake_file.tar.gz', extract_path='./')
+
+        mocked_tar_gz.extractall.assert_called_once_with('./')
+
+    @patch('pkg.os.file.type.tar_gz.extract.open_tarfile')
+    def test_if_the_file_was_extracted_successful(self, mock_open_tarfile):
+        """
+        Return true if the file was extracted successful.
+        """
+        mock_open_tarfile.return_true = True
+
+        extracted = extract(path='fake_file.tar.gz', extract_path='./')
+
+        self.assertEqual(True, extracted)
